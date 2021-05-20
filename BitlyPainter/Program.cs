c2d9e9a09d02de276462c6507dcf679f812dc9ef
freakyshine <p.daniel@medalu.ch>
2021-05-20 10:20:49 +0200

    Adding Canvas class with necessary properties and fields, methods and constructors

--- BitlyPainter/Program.cs
+++ BitlyPainter/Program.cs
@@ -10,6 +10,11 @@ namespace BitlyPainter
     {
         static void Main(string[] args)
         {
+            // The main canvas. Also the only canvas, there is no other canvas.
+            Canvas BitlyCanvas = new Canvas();
+
+
+
             Console.WriteLine("### Welcome to Bitly Painter ###");
             AudibleExit();
         }
@@ -21,6 +26,43 @@ namespace BitlyPainter
         {
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// Represents a canvas object, it has typicall properties of a canvas, such as dimensions and color
+    /// </summary>
+    class Canvas
+    {
+        /// <summary>
+        /// Stores RGB values in a Byte array. Values from 0-255 are valid. Array stores 3 values.
+        /// </summary>
+        public byte[] Color { get; set; }
+        public byte[] BackgroundColor { get; set; }
+        /// <summary>
+        /// Stores the dimensions of the canvas. Values from 0-255 are valid. Array stores 2 values.
+        /// </summary>
+        private byte[] Dimensions { get; set; } = new byte[2];
+        /// <summary>
+        /// Xsize changes the values of the private field Dimensions. Valid values are 0-255. Set to a higher or lower value, the actual value will be 0.
+        /// </summary>
+        public int Xsize { get { return Dimensions[0]; } set { _ = value < 0 || value > 255 ? Dimensions[0] = 0 : Dimensions[0] = (byte)value; } }
+        /// <summary>
+        /// Ysize  changes the values of the private field Dimensions. Valid values are 0-255. Set to a higher or lower value, the actual value will be 0.
+        /// </summary>
+        public int Ysize { get { return Dimensions[1]; } set { _ = value < 0 || value > 255 ? Dimensions[1] = 0 : Dimensions[1] = (byte)value; } }
+
+        /// <summary>
+        /// Constructs a Canvas with given X and Y size.
+        /// </summary>
+        /// <param name="x">Horizontal size</param>
+        /// <param name="y">Vertical size. Valid numbers</param>
+        public Canvas()
+        {
+            // Defining a new byte array and setting it's maximum to 3 values. Setting default color to black
+            Color = new byte[3] { 0, 0, 0};
+            // Defining a new byte array and setting it's maximum to 3 values. Setting default backgroundcolor to white
+            BackgroundColor = new byte[3] { 255, 255, 255 };
         }
     }
 }
